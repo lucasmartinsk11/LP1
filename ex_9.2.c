d76cab83c9a135de7c3bb96456543ebd439c004b
lucasmartinsk11 <40902668+lucasmartinsk11@users.noreply.github.com>
2018-12-10 10:09:38 -0200

    Create ex_9.2.c

---
+++ ex_9.2.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+
+typedef struct box {
+		int valor;
+		struct box* prox;
+		
+	}caixa;
+
+int main(){
+	
+	caixa c1;
+	caixa c2;
+	caixa c3;
+	caixa c4;
+	caixa c5;
+	
+	c1.valor = 1;
+	c1.prox = &c2;
+	
+	c2.valor = 2;
+	c2.prox = &c3;
+	
+	c3.valor = 3;
+	c3.prox = &c4;
+	
+	c4.valor = 4;
+	c4.prox = &c5;
+	
+	c5.valor = 5;
+	c5.prox = &c1;
+	
+	printf("%d --> %d --> %d --> %d --> %d --> %d\n", c1.valor,(*c1.prox).valor,(*c2.prox).valor,(*c3.prox).valor,(*c4.prox).valor,(*c5.prox).valor);
+}
