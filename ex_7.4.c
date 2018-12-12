a7b9fa80dbddff537c7e29dba9af1707cc317a0c
lucasmartinsk11 <40902668+lucasmartinsk11@users.noreply.github.com>
2018-12-12 14:35:55 -0200

    Update ex_7.4.c

--- ex_7.4.c
+++ ex_7.4.c
@@ -10,18 +10,29 @@ typedef struct persona{
 	int pontos;
 	ponto c;
 	}personagem;
-	
-void preencher_pontos(ponto* pp);
+
+void preencher_personagem(personagem* p);	
+void preencher_pontos(personagem* pp);
 void preencher(ponto* p);
 
 int main(){
 	personagem p[10];
+	preencher_pontos(&p);
 }
 	
-void preencher_pontos(ponto* pp){
-	for(int i = 0; i < 10; i++){
-		preencher(&(*(pp+i)));
+void preencher_pontos(personagem* pp){
+	int i;
+	for(i = 0; i < 10; i++){
+		preencher_personagem(&pp[i]);
 	}
+}
+void preencher_personagem(personagem* p){
+	int i;	
+	(*p).identidade = i+1;
+	printf("Digite a pontuacao do personagem %d: ",(*p).identidade);
+	scanf("%d",&(*p).pontos);
+	preencher(&(*p).c);
+	i++;
 }
 void preencher(ponto* p){
 	printf("Coordenada x: ");
