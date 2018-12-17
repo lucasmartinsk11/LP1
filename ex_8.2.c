f3d4264202dbf81b8a5435dd8243805cef78fb4b
lucasmartinsk11 <40902668+lucasmartinsk11@users.noreply.github.com>
2018-12-16 23:52:10 -0200

    Update ex_8.2.c

--- ex_8.2.c
+++ ex_8.2.c
@@ -7,13 +7,11 @@ int main(){
 	char string[25];
 	
 	FILE* f = fopen("/home/lucas/Área de Trabalho/ex_8.1.txt","r");
-	printf("estouaqui - 1\n");
 	for(int i = 0;; i++){
 		
 		string[i] = fgetc(f);
 		if(string[i] == '\0'){break;}
 	}
-	printf("estouaqui - 2\n");
 	printf("%s",string);
 	fclose(f);
 }
